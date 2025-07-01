370ca0558933b9f0584a9e348ee696c4aa1bcaf9
Yamilet <deya.meme.08@gmail.com>
2025-07-01 12:52:07 -0500

    REPORTE 2

---
+++ app/reports/reporte-cotizacion-2.php
@@ -0,0 +1,22 @@
+<?php
+require_once '../../vendor/autoload.php';
+use Spipu\Html2Pdf\Html2Pdf;
+use Spipu\Html2Pdf\Exception\Html2PdfException;
+use Spipu\Html2Pdf\Exception\ExceptionFormatter;
+
+try {
+    ob_start();
+    require_once './content/data-reporte-cotizacion-2.html';
+    $html = ob_get_clean();
+
+    // Inicializo en español y habilito imágenes locales
+    $html2pdf = new Html2Pdf('P', 'A4', 'es', true, 'UTF-8', [0, 0, 0, 0]);
+    $html2pdf->pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+    $html2pdf->writeHTML($html);
+    $html2pdf->output('reporte-cotizacion.pdf');
+} catch (\Exception $e) {
+    $html2pdf->clean();
+
+    $formatter = new ExceptionFormatter($e);
+    echo $formatter->getHtmlMessage();
+}
